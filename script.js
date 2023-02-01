a06a04104b7097e664a5d2489d587ba0dd187d7b
Kyle <kylemphillips65@yahoo.com>
2023-01-31 19:39:08 -0500

    fixed the save button function

--- script.js
+++ script.js
@@ -1,31 +1,19 @@
 
-$(function () {
+$(function dynamicPage() {
+
+ 
+  var saveBtn = $('.saveBtn');
+  var time = dayjs().hour();
+  var date = $('#today');
+  date.text(new Date());
   
-  // <--- How I got the date at the top of my page and to console log into the console--->
-  // <--- to change the time you can use this and add it to the end of dayjs(): .add(-10, 'hour'), you can move forward with positive numbers and move backwards with negative numbers--->
-  var whatItsTime = dayjs().add(-5, 'hour').hour();
-  console.log(whatItsTime);
-$('#whatIsToDay').text(dayjs().format('dddd MMMM Do YYYY'))
-
-
-// <--- This is the function I use to get my "Time blocks " aka meeting times to change colors for the time--->
-$('.meeting-time').each(function() {
-  var thisIsTime = parseInt($(this).attr('id').split('-')[1]);
-  if (thisIsTime < whatItsTime) {
-    $(this).addClass('past');
-  } else if (whatItsTime === thisIsTime) {
-    $(this).addClass('present');
-  } else if (thisIsTime > whatItsTime) {
-    $(this).addClass('future');
-  }
-});
-
-var saveBtn = $('.saveBtn');
-  $(saveBtn). click(function() {
+  // whatever the user types and clicks the save button, then the data is saved in local storage
+  $(saveBtn).click(function() {
     var scheduleInput = $(this).siblings('.description').val();
     var hour = $(this).parent().attr('id');
     localStorage.setItem(hour, scheduleInput);
   })
-
+  
+  
 });
-
+})
