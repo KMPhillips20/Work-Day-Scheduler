5620695cbbc91c8be346dd18f9bd0cdf95c0bd5c
Kyle <kylemphillips65@yahoo.com>
2023-01-31 19:45:37 -0500

    fixed the save button and also added the color change of the time blocks

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ $(function dynamicPage() {
   var date = $('#today');
   date.text(new Date());
   
+  
   // whatever the user types and clicks the save button, then the data is saved in local storage
   $(saveBtn).click(function() {
     var scheduleInput = $(this).siblings('.description').val();
@@ -14,6 +15,21 @@ $(function dynamicPage() {
     localStorage.setItem(hour, scheduleInput);
   })
   
+  
+
+  //  this function will change the color of the back ground depending on what time it is 
+  // past = grey
+  // current hour = red
+  // future = green
+  $('.description').each(function(){
+    var meetingBlock = parseInt($(this).parent().attr('id').replace('hour', ''));
+    if (mettingBlock < time){
+    $(this).parent().addClass('past')
+  } else if(meetingBlock === time) {
+    $(this).parent().addClass('present')
+  } else {
+    $(this).addClass('future');
+  }
   
 });
 })
